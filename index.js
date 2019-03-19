72ff8dea6b19dad9952b3bbf8977793db51e19b1
金林成宇 <jinlinchengyu@meituan.com>
2019-03-19 19:50:14 +0800

    Revert "git reset b53e4a62397"
    This reverts commit 95e692db4907b55f57b262578248174e806e9068.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import EGFour from './app/EGFour/index'
 import EGFive from './app/EGFive/index'
 import EGSix from './app/EGSix/index'
 import EGSeven from './app/EGSeven/index'
+import FlexLayout from './app/FlexLayout/index'
 import './index.scss'
 
 ReactDOM.render((
@@ -22,5 +23,6 @@ ReactDOM.render((
     <Route path="/egfive" component={EGFive} />
     <Route path="/egsix" component={EGSix} />
     <Route path="/egseven" component={EGSeven} />
+    <Route path="/flex-layot" component={FlexLayout} />
   </Router>
 ), document.getElementById('root'))
